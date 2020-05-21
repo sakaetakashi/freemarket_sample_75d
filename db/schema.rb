d40904a22a4c8dc29171d36a53536beeb98b0301
shirokuma50460 <koshiro0606.coochan-2222@ezweb.ne.jp>
2020-05-21 19:28:21 +0900

    ancestry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_110555) do
+ActiveRecord::Schema.define(version: 2020_05_21_063920) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "post_code"
@@ -25,11 +25,11 @@ ActiveRecord::Schema.define(version: 2020_05_20_110555) do
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "genre"
-    t.string "subgenre"
-    t.string "detail"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
