0a548e39297f7edb040c43bca587d6dd295b76f4
n-ogino <n.ogino.jp@gmail.com>
2020-05-24 17:09:58 +0900

    user_part1

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root 'products#index'
+  root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources  :products
   resources  :categories
