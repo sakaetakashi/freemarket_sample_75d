886cc6af6bc13884e931868d6ebd30867b919d62
n-ogino <n.ogino.jp@gmail.com>
2020-05-27 14:33:17 +0900

    purchasse_repair1

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
 
-  root 'products#purchase'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources  :products do
     member do
