b9af5172831502c524177110093268fe0c8e1b87
kentomiyazawa <kento4241@yahoo.co.jp>
2020-06-02 17:56:17 +0900

    comit

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,9 +7,10 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :buyer, class_name: "User", optional: true
 
-  validates :user_id, :category_id, :condition, :arrive_at, :price, :shipping_fee, :region_id,  presence: true
+  validates :user_id, :category_id, :condition, :arrive_at, :shipping_fee, :region_id,  presence: true
   validates :product_name, presence: true, length: { in: 1..40 }
   validates :explain, presence: true, length: { in: 1..1000 }
+  validates :price, numericality: { greater_than_or_equal_to: 50 }
   validates :images, presence: true
 
 end
