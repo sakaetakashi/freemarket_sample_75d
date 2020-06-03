4d9dfeaee55379ac2202f1917c9da14b42f9f616
n-ogino <n.ogino.jp@gmail.com>
2020-06-03 19:34:08 +0900

    migration_delete

--- db/migrate/20200603090447_remove_s_first_name_kana_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSFirstNameKanaFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :s_first_name_kana, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_090544) do
+ActiveRecord::Schema.define(version: 2020_06_03_075920) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "post_code", null: false
